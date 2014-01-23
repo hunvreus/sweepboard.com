7f575230c913c7322521c93363900425f74e04e0
Ronan Berder <ronan@wiredcraft.com>
2014-01-22 21:43:15 -0800

    Initial commit

---
+++ compass.config.rb
@@ -0,0 +1,7 @@
+css_path = 'assets/css'
+sass_path = 'assets/scss'
+images_path = 'assets/images'
+fonts_path = 'assets/fonts'
+output_style = :compressed
+line_comments = false
+# sass_options = { :debug_info => true }
---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "vendor": {
+        "js": [
+            "vendor/jquery/jquery.js",
+            "vendor/highlightjs/highlight.pack.js",
+            "vendor/Swipe/swipe.js"
+        ],
+        "css": [
+            "vendor/normalize-css/normalize.css",
+            "vendor/highlightjs/styles/monokai.css"
+        ]
+    },
+    "custom": {
+        "js": [
+            "assets/js/**/*.js"
+        ],
+        "css": [
+            "assets/css/**/*.css"
+        ],
+        "scss": [
+            "assets/scss/**/*.{scss,sass}"
+        ]
+    }
+}
