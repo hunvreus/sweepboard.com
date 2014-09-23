2c6af0c97df0df533792d3a76b8214073916afd9
fraserxu <xvfeng123@gmail.com>
2014-09-23 12:35:59 +0800

    rebuild all the thing

--- README.md
+++ README.md
@@ -2,5 +2,15 @@ This is the public facing website for [sweepboard.com](http://sweepboard.com).
 
 ## Install
 
-1. `npm install`
-2. `bower install`
+`npm install`
+`bower install`
+
+
+## Build
+
+`gulp`
+
+## Publish
+
+`bash build.sh`
+
--- build.sh
+++ build.sh
@@ -1,40 +1,9 @@
 #!/bin/bash
-###################
-# Build / deploy script
-# Meant to be called by devops builder
-###################
 
-COMMIT_MSG=$1
-if [ -z "$COMMIT_MSG" ]; then
-    # default commit message
-    COMMIT_MSG='New deployment'
-fi
+# clean
+rm atom.xml CNAME google945dae66a3fbd963.html index.html robots.txt sitemap.xml styles.css
+rm -r 404/ faq/ privacy/ tos/
 
-TMP_FOLDER=$(mktemp -d)
-
-#
-# Build process
-#
-npm --silent install
-#bower --silent install
-gulp
-
-# Copy build files
-cp -a _site/* $TMP_FOLDER
-
-#
-# Deployment
-#
-
-# Stash changes to allow branch switch
-git stash
-git checkout gh-pages
-git clean -f -d
-git clean -f -x
-cp -a $TMP_FOLDER/* .
-git add .
-git commit -am "$COMMIT_MSG"
-git push
-
-# Cleanup
-rm -rf $TMP_FOLDER
+# copy
+cp -r _site/* ./
+rm -r _site
